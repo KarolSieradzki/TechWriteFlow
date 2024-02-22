f32cf72501c63306f47b7aa7dacc23128e7b6fbf
KarolSieradzki <karol.sieradzki.2000@gmail.com>
2024-02-22 21:18:01 +0100

    create shared module and shell component

--- TechWriteFlow/src/app/app.module.ts
+++ TechWriteFlow/src/app/app.module.ts
@@ -5,13 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { SharedModule } from './shared/shared.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule
   ],
   providers: [
     provideClientHydration(),
