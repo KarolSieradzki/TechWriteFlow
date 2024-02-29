420c1509c7c947c3c295da94a7e87581166c130f
KarolSieradzki <karol.sieradzki.2000@gmail.com>
2024-02-29 17:55:52 +0100

    creating new blog and portfolio when new user logged

--- TechWriteFlow/src/app/services/auth/auth.service.ts
+++ TechWriteFlow/src/app/services/auth/auth.service.ts
@@ -5,6 +5,8 @@ import firebase from 'firebase/compat/app';
 import { User } from '../../shared/firestore-models/user.model';
 import { Router } from '@angular/router';
 import { Observable, of, switchMap } from 'rxjs';
+import { Portfolio } from '../../shared/firestore-models/portfolio.model';
+import { Blog } from '../../shared/firestore-models/blog.model';
 
 @Injectable({
   providedIn: 'root'
@@ -16,8 +18,9 @@ export class AuthService {
   constructor(
     private router: Router,
     private afAuth: AngularFireAuth,
-    private firestore: AngularFirestore
-  ) { 
+    private firestore: AngularFirestore,
+
+  ) {
     this.user$ = this.afAuth.authState.pipe(switchMap(user => {
       //if logged in
       if (user) {
@@ -30,8 +33,12 @@ export class AuthService {
     }))
   }
 
-  userCollection: AngularFirestoreCollection<User> = this.firestore.collection<User>("User");
-
+  userCollection: AngularFirestoreCollection<User> =
+    this.firestore.collection<User>("User");
+  portfolioCollection: AngularFirestoreCollection<Portfolio> =
+    this.firestore.collection<Portfolio>("Portfolio");
+  blogCollection: AngularFirestoreCollection<Blog> =
+    this.firestore.collection<Blog>("Blog");
   /*
     Logs into a google account, if the account does not yet exist creates:
     - new user document 
@@ -42,20 +49,20 @@ export class AuthService {
     const provider = new firebase.auth.GoogleAuthProvider();
     const credential: any = await this.afAuth.signInWithPopup(provider);
 
-    if (!(await this.userExist(credential.user.uid))){
+    if (!(await this.userExist(credential.user.uid))) {
+      await this.createEmptyBlog(credential.user);
+      await this.createEmptyPortfolio(credential.user);
       await this.createUserDocument(credential.user);
-
-      // TODO
 
       this.router.navigate(['/profile-settings/alias']);
       return;
     }
-      
+
     this.router.navigate(['/admin-panel']);
   }
 
-  async userExist(uid: string):Promise<boolean>{
-    return new Promise((resolve)=>{
+  async userExist(uid: string): Promise<boolean> {
+    return new Promise((resolve) => {
       this.userCollection.ref.doc(uid).get().then((docSnapshot) => {
         if (docSnapshot.exists) resolve(true);
         else resolve(false);
@@ -66,8 +73,8 @@ export class AuthService {
   /*
     Creates a user document with id identical to account id 
   */
-  async createUserDocument(user:any){
-    let userData: User={
+  private async createUserDocument(user: any) {
+    let userData: User = {
       uid: user.uid,
       email: user.email,
       displayName: user.displayName,
@@ -77,8 +84,42 @@ export class AuthService {
 
     await this.userCollection.ref.doc(user.uid)
       .set(userData, { merge: true })
-      .catch((error)=>{
+      .catch((error) => {
         console.error("Error while creating user document: ", error);
+      })
+  }
+
+  /*
+  *  Creates a new Portfolio document with id identical to account id 
+  */
+  private async createEmptyPortfolio(user: any) {
+    let portfolio: Portfolio = {
+      uid: user.uid,
+      public: false,
+      projects: []
+    }
+
+    await this.portfolioCollection.ref.doc(user.uid)
+      .set(portfolio, { merge: true })
+      .catch((error) => {
+        console.error("Error while creating portfolio document: ", error);
+      })
+  }
+
+  /*
+  *  Creates a new Blog document with id identical to account id 
+  */
+  private async createEmptyBlog(user: any) {
+    let blog: Blog = {
+      uid: user.uid,
+      public: false,
+      posts: []
+    }
+
+    await this.blogCollection.ref.doc(user.uid)
+      .set(blog, { merge: true })
+      .catch((error) => {
+        console.error("Error while creating blog document: ", error);
       })
   }
 
