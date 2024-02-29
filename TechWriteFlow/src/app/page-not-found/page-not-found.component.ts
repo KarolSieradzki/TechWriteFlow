24f7e1429c92cf8cf70b6d82c2caaadb37a6db6a
KarolSieradzki <karol.sieradzki.2000@gmail.com>
2024-02-29 17:05:42 +0100

    update page not found component

--- TechWriteFlow/src/app/page-not-found/page-not-found.component.ts
+++ TechWriteFlow/src/app/page-not-found/page-not-found.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.scss'
 })
-export class PageNotFoundComponent {
+export class PageNotFoundComponent implements OnInit{
+  
+  text: string = "404, page not found.";
+  displayedText: string = "";
+  private currentIndex: number = 0;
 
+  ngOnInit(): void {
+    this.typeNextLetter();
+  }
+
+  private typeNextLetter() {
+    if (this.currentIndex < this.text.length) {
+      this.displayedText += this.text[this.currentIndex++];
+      setTimeout(() => this.typeNextLetter(), 100); 
+    }
+  }
 }
